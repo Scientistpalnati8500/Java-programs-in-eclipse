43884943c63ea255be09d3569c57f11db3e1dac9
balas <scientistpalnati@gmail.com>
2023-02-04 19:24:29 +0530

    bye

---
+++ src/java_loop_statements/Java_While_Loop.java
@@ -0,0 +1,22 @@
+package java_loop_statements;
+
+public class Java_While_Loop {
+	public static void main(String args[])
+	{
+		int a =0;
+		
+		/*
+	  	 		while (condition) {
+  					// looping code block to be executed
+  					//increment/decrement
+				} 
+		  */
+		
+		while(a<=101)
+		{
+			System.out.println(a);
+			a++;
+		}
+	}
+
+}
---
+++ src/java_loop_statements/Java_do_while_loop.java
@@ -0,0 +1,5 @@
+package java_loop_statements;
+
+public class Java_do_while_loop {
+
+}
