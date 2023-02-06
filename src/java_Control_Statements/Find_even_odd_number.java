0d212623f9500b45c58e2c75609ec46fe559085d
balas <scientistpalnati@gmail.com>
2023-02-06 14:11:27 +0530

    today 6th jan

---
+++ src/java_Control_Statements/Find_even_odd_number.java
@@ -0,0 +1,18 @@
+package java_Control_Statements;
+
+public class Find_even_odd_number {
+	public static void main(String args[])
+	{
+		int i =11;
+		
+		if(i%2 == 0)
+		{
+			System.out.println("this is even number");
+		}
+		else
+		{
+			System.out.println("othervice its only odd number");
+		}
+	}
+
+}
---
+++ src/java_Control_Statements/Find_odd_even_number.java
@@ -0,0 +1,19 @@
+package java_Control_Statements;
+
+public class Find_odd_even_number {
+	public static void main(String args[])
+	{
+		int sum = 21;
+		
+		if(sum%2 == 1)
+		{
+			System.out.println("this is odd number");
+		}
+		else
+		{
+			System.out.print("this not odd number,so its even number");
+		}
+	}
+	
+
+}
